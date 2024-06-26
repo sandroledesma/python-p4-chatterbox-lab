d4a2064e17f3da76590e648be70363026e908b7a
Alessandro Ledesma <sandroledesma@gmail.com>
2024-06-26 15:10:43 -0400

    update files

--- server/app.py
+++ server/app.py
@@ -14,13 +14,46 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        msgs = Message.query.order_by(Message.created_at).all()
+    
+        return [msg.to_dict() for msg in msgs], 200
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_msg = Message(
+            username=data.get('username'), 
+            body=data.get('body')
+        )
+        db.session.add(new_msg)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        return new_msg.to_dict(), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    msg = Message.query.filter(Message.id == id).first()
+
+    if not msg: 
+        return {'error': 'message not found'}, 404
+    
+    if request.method == 'PATCH':    
+        data = request.get_json()
+
+        msg.body = data.get('body')
+
+        db.session.add(msg)
+        db.session.commit()
+
+        return msg.to_dict(), 200
+
+    elif request.method == 'DELETE':
+        db.session.delete(msg)
+        db.session.commit()
+
+        return {}, 204
 
 if __name__ == '__main__':
     app.run(port=5555)
---
+++ server/migrations/versions/c536699ca0fa_update_files.py
@@ -0,0 +1,24 @@
+"""update files
+
+Revision ID: c536699ca0fa
+Revises: cb4aaa02df20
+Create Date: 2024-06-26 15:04:56.506322
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c536699ca0fa'
+down_revision = 'cb4aaa02df20'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
